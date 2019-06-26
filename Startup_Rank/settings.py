50e52306ae311fc2bdd25d9fe0232d3056dcc1e9
Ion <ionpetro@gmail.com>
2019-06-26 15:48:18 +0300

    Fix manage.py

--- Startup_Rank/settings.py
+++ Startup_Rank/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'StartupRank.urls'
+ROOT_URLCONF = 'Startup_Rank.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'StartupRank.wsgi.application'
+WSGI_APPLICATION = 'Startup_Rank.wsgi.application'
 
 
 # Database
