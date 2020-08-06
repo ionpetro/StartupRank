0c993f102eabe0d81eec2cf141ff1f8bf832482c
ionpetro <ionpetro@gmail.com>
2020-08-06 22:32:37 +0300

    fix minor syntax error

--- Startup_Rank/settings.py
+++ Startup_Rank/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['DEFAULT'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 
 
