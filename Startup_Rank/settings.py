845a0bb2848b87126c15c55f4d1f8ff4e2a3cd9f
ionpetro <ionpetro@gmail.com>
2020-08-06 22:06:10 +0300

    add dj-database-url

--- Startup_Rank/settings.py
+++ Startup_Rank/settings.py
@@ -138,3 +138,6 @@ STATICFILES_DIRS = [
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
