80f071cfa7c0beb6c55870e7c8d89f2e02649b80
ionpetro <ionpetro@gmail.com>
2020-08-06 21:19:00 +0300

    init posgresql

--- Startup_Rank/settings.py
+++ Startup_Rank/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'Startup_Rank.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'startuprank',
+        'USER': 'ionpetro',
+        'PASSWORD': 'thisiskappa69',
+        'HOST': 'localhost',
+        'PORT': '5433',
+
     }
 }
 
---
+++ env/Lib/site-packages/vote/__init__.py
@@ -0,0 +1,3 @@
+VERSION = (2, 2, 0)
+
+default_app_config = 'vote.apps.VoteAppConfig'
